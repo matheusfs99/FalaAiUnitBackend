7cb07590f4482749ddec4ae95db9202b86ecfaf1
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-10-07 09:44:16 -0300

    corrige queryset para filtrar os meetings dos guests

--- apps/meetings/views.py
+++ apps/meetings/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Meeting
 from .serializers import MeetingSerializer
 from datetime import datetime
+from django.db.models import Q
 
 class MeetingViewSet(viewsets.ModelViewSet):
     queryset = Meeting.objects.all()
@@ -11,7 +12,7 @@ class MeetingViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        return self.queryset.filter(owner=user, end_time__gt=datetime.now())
+        return self.queryset.filter(Q(owner=user) | Q(guest=user), end_time__gt=datetime.now())
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
